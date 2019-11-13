102be2acb1cd2e74c941ac24e243a697935483a4
EleniDriva <41011966+elenidriva@users.noreply.github.com>
2019-11-13 11:37:20 +0200

    Added negative numbers (in Tokens) | Added Test data
    Negative numbers added in 'Tokens' | Added a file with some input to help us understand if our grammar is working as expected

---
+++ Input Examples in Python/minipythonexample.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+#A miniPython example
+
+#! /usr/bin/env python
+#A miniPython example
+
+if x <3+2:
+	print 3+2*3*4
+    
+a = [1,2,3] # Check List
+z = 2**2+2
+b = -2 # Check negative numbers. Need to update Tokens or Productions
+
+def add(e,f):
+    return e + f
+    
+add(b,z)
+
